a4ecac0cbd0c711a30cab24a2653a5a6e15c4fac
Alexander Aguilar <alexander.aguilar.torrico@gmail.com>
2023-12-11 19:55:20 -0400

    Add support to Ubuntu Local and RedHat Local

---
+++ deploy.redhat.local.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+  
+echo "Init creating ssh key to verify conectivity...."
+
+sudo dnf install -y ansible git
+
+ssh-keygen -q -b 2048 -t rsa -N "" -C "creating SSH" -f KEY-UBUNTU
+
+chmod 400 KEY-REDHAT
+
+cat KEY-REDHAT.pub >> ~/.ssh/authorized_keys
+
+git clone https://github.com/alexolomeo/nextcloud.server.git
+
+ansible -i hosts -m ping all
+
+ansible-playbook -i hosts deploy.yml -v
+
